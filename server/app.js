69e8a0e99dc8a3d40ca55520208736894fcfb950
aakarsh890 <162731300+aakarsh890@users.noreply.github.com>
2025-05-22 09:52:16 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 
 if(process.env.NODE_ENV !== "production"){
@@ -13,12 +12,6 @@ if(process.env.NODE_ENV !== "production"){
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit:"50mb", extended: true }));
 app.use(cookieParser());
-
-app.use(cors({
-    origin: ["http://localhost:3000", "https://your-frontend.vercel.app"], // Add your frontend URLs
-    credentials: true, // Allows cookies
-}));
-app.options("*", cors());
 
 
 //importing routes
